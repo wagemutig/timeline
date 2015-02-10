4a164173359fefbfc96ae601328593e3a1c79721
mala23 <mail@mala23.me>
2015-02-10 18:38:03 +0100

    add alot of content and style some

--- server.rb
+++ server.rb
@@ -14,8 +14,28 @@ get '/timeline' do
   haml :timeline
 end
 
+get '/gbm/zeitgeist' do
+  haml :zeitgeist
+end
+
 get '/gbm/aguacu' do
   haml :aguacu
+end
+
+get '/oi/migros' do
+  haml :migros
+end
+
+get '/oi/lightbox' do
+  haml :lightbox
+end
+
+get '/ma/lecture-voter' do
+  haml :'lecture-voter'
+end
+
+get '/ma/node-rover' do
+  haml :'node-rover'
 end
 
   #start the server if ruby file executed directly
