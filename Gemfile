2ad76088d8d249d73e24edbe45625406042a64f7
mala23 <mail@mala23.me>
2015-02-10 21:27:44 +0100

    use sass, compass and some styling

--- Gemfile
+++ Gemfile
@@ -6,4 +6,5 @@ group :development do
   gem 'haml'
   gem 'sass'
   gem 'compass'
+  gem 'autoprefixer-rails'
 end
---
+++ config.rb
@@ -0,0 +1,14 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+require 'autoprefixer-rails'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/app/public/styles/css"
+sass_dir = "/app/public/styles/sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+sourcemap = true
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :compressed
